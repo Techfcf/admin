d41f5248fe2b46592bae874d7cbbf6a316cafa85
Techfcf <tech@fcfindia.in>
2024-10-08 01:44:34 +0530

    Update orthomoasicimage.tsx

--- src/components/HomePage/Home/orthomoasicimage.tsx
+++ src/components/HomePage/Home/orthomoasicimage.tsx
@@ -61,6 +61,7 @@ const OrthoMosaicImage: React.FC = () => {
           'Referer': 'https://admin.fitclimate.com',
         }
       });
+       console.log("data",response.statusText);
 
       if (!response.ok) {
         console.error('Tree count API call failed:', response.statusText);
